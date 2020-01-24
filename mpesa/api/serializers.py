a024413a7e06e108f4e89bcc384479728a7b5048
kamauvick <kamauvick@outlook.com>
2020-01-25 00:21:38 +0300

    Add fields to the LNMOnline serializer.

--- mpesa/api/serializers.py
+++ mpesa/api/serializers.py
@@ -4,4 +4,14 @@ from mpesa.models import LNMOnline
 class LNMOnlineSerializer(serializers.ModelSerializer):
     class Meta:
         model = LNMOnline
-        fields = ('id', )
+        fields = (
+            'id', 
+            'merchant_request_id', 
+            'checkout_request_id',
+            'result_code',
+            'result_desc',
+            'amount',
+            'mpesa_receipt_number',
+            'balance',
+            'transaction_date',
+            'phonenumber', )
