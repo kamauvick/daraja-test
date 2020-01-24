1bc3e32d0e108dfdf604a102bfd32e7547fbaa9a
kamauvick <kamauvick@outlook.com>
2020-01-25 01:27:28 +0300

    Format how the transactions are displayed on the admin page.

--- mpesa/admin.py
+++ mpesa/admin.py
@@ -2,4 +2,23 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(LNMOnline)
+
+
+@admin.register(LNMOnline)
+class LNMOnline(admin.ModelAdmin):
+
+    list_display = ('id',
+                    'merchant_request_id',
+                    'checkout_request_id',
+                    'result_code',
+                    'result_desc',
+                    'amount',
+                    'mpesa_receipt_number',
+                    'balance',
+                    'transaction_date',
+                    'phonenumber', )
+
+    list_filter = ('id', 'phonenumber', 'mpesa_receipt_number',)
+
+    ordering = ("id", 'transaction_date',)
+
