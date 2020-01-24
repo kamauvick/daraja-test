9a7e6a82f6797ce7398995ca765875fe0111bdc4
kamauvick <kamauvick@outlook.com>
2020-01-24 22:40:29 +0300

    Comment out all the view functions in the mpesa views.py

--- mpesa/views.py
+++ mpesa/views.py
@@ -1,36 +1,22 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import login, authenticate
+# from django.contrib.auth.decorators import login_required
 
-from .forms import  ProfileUpdateForm
-from django.contrib.auth.forms import UserCreationForm
+# from .forms import  ProfileUpdateForm
+# from django.contrib.auth.forms import UserCreationForm
 
+# def signup(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             user.refresh_from_db()  # load the profile instance created by the signal
+#             user.save()
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=user.username, password=raw_password)
+#             login(request, user)
+#             return redirect('/login')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'registration/register.html', locals())
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-            return redirect('/login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/register.html', locals())
-
-@login_required
-def index(request):
-    return render(request, 'main/landing.html', locals())
-
-
-def list_products(request):
-    pass
-
-def product_view(request):
-    pass
-
-def checkout_product(request):
-    pass
