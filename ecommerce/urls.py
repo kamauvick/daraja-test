24e199abc91958a1a3181b5b0199d70db52fa025
kamauvick <kamauvick@outlook.com>
2020-01-24 22:29:36 +0300

    Include mpesa.api urls.

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mpesa.urls')),
+    path('api/auth', include('mpesa.urls')),
+    path('api/payments', include('mpesa.api.urls')),
 ]
