5a6b3b908600ca29a94a3a07b2857f6073ca08e2
kamauvick <kamauvick@outlook.com>
2020-01-24 22:35:00 +0300

    Add a view function for Lipa na mpesa.

---
+++ mpesa/api/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from mpesa.api.serializers import LNMOnlineSerializer
+from mpesa.models import LNMOnline
+
+class LNMCallbackUrlApiView(CreateAPIView):
+    queryset = LNMOnline.objects.all()
+    serializer_class = LNMOnlineSerializer
+    # permission_classes = ['AllowAny',]
+
+    def create(self, request, *args, **kwargs):
+        # return super().create(request, *args, **kwargs)
+        print(request.data, 'this is the request data.')
