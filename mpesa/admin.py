e624e665add9c1fe4af6a6818bf0bfe12303612a
kamauvick <kamauvick@outlook.com>
2020-01-25 01:39:57 +0300

    Trim the number of fields displayed on the admin interface.

--- mpesa/admin.py
+++ mpesa/admin.py
@@ -8,15 +8,11 @@ admin.site.register(Profile)
 class LNMOnline(admin.ModelAdmin):
 
     list_display = ('id',
-                    'merchant_request_id',
-                    'checkout_request_id',
-                    'result_code',
-                    'result_desc',
+                    'phonenumber',
                     'amount',
                     'mpesa_receipt_number',
-                    'balance',
                     'transaction_date',
-                    'phonenumber', )
+                     )
 
     list_filter = ('id', 'phonenumber', 'mpesa_receipt_number',)
 
