44dd84190b43e979e87cabb2746a5a8f5ce7c159
kamauvick <kamauvick@outlook.com>
2020-01-25 13:24:47 +0300

    Create a client for the mpesa apis.

---
+++ duka/README.md
@@ -0,0 +1,2 @@
+# My Duka client webapp
+    A simple ecommerce webapp to consume the apis from the mpesa app. 
---
+++ duka/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+
+from mpesa.models import Profile
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', )
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('username', 'profile_pic', 'email', 'phone_number', )
