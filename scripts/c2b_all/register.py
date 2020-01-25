8a2f0113011a3ca02a918852025c2f883c991934
kamauvick <kamauvick@outlook.com>
2020-01-25 15:06:17 +0300

    Fix callback urls on the c2b.py file.

--- scripts/c2b_all/register.py
+++ scripts/c2b_all/register.py
@@ -9,9 +9,12 @@ def register_url():
     headers = {"Authorization": "Bearer %s" % my_access_token}
     request = {"ShortCode": c2b_shortcode,
                "ResponseType": "Completed",
-               "ConfirmationURL": "https://darajavick.herokuapp.com/api/payments/validation_url/",
-               "ValidationURL": "https://darajavick.herokuapp.com/api/payments/confirmation_url/"}
+               "ConfirmationURL": "https://darajavick.herokuapp.com/api/payments/confirmation_url/",
+               "ValidationURL": "https://darajavick.herokuapp.com/api/payments/validation_url/"}
 
     response = requests.post(api_url, json=request, headers=headers)
 
     print(response.text)
+
+
+
