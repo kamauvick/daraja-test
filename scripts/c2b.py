b8b33ab43f45a0ac779456db0760d9f0bac0f327
kamauvick <kamauvick@outlook.com>
2020-01-25 14:47:22 +0300

    Comment out the validation function from the c2b.py file.

--- scripts/c2b.py
+++ scripts/c2b.py
@@ -3,7 +3,7 @@ from c2b_all import register, simulate
 
 # Run register_url function
 
-register.register_url() # Only done once
+# register.register_url() # Only done once
 
 # Run Simulate_c2b_transaction 
 
