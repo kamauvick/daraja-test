4ef02b0f9cc15819bd7571545932209268bdc5b0
kamauvick <kamauvick@outlook.com>
2020-01-26 18:00:54 +0300

    Remove payment endpoints from the /api/payments/ path.

--- mpesa/urls.py
+++ mpesa/urls.py
@@ -2,11 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, re_path,include
 from django.contrib.auth import views
+from rest_framework.routers import DefaultRouter
+from .views import MakeLNMPayment, MakeC2BPayment
+
+router = DefaultRouter()
+router.register('LNMPayment', MakeLNMPayment)
+router.register('C2BPayment', MakeC2BPayment)
+
 
 urlpatterns = [
     path('auth/', include('rest_auth.urls')),
-    path('registration/', include('rest_auth.registration.urls'))
-
+    path('registration/', include('rest_auth.registration.urls')),
+    path('', include(router.urls)),
 ]
 
 if settings.DEBUG:
